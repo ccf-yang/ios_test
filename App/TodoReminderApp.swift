50b0203e27199995856a690d8ba7e56a37fc2f79
ccf-yang <32785990+ccf-yang@users.noreply.github.com>
2026-07-23 22:01:08 +0800

    Update TodoReminderApp.swift

--- App/TodoReminderApp.swift
+++ App/TodoReminderApp.swift
@@ -2,8 +2,8 @@ import SwiftUI
 
 @main
 struct TodoReminderApp: App {
-    // 初始化全局数据库控制器
-    @StateObject private var persistenceController = PersistenceController()
+    // 修改点：不再使用 @StateObject
+    private let persistenceController = PersistenceController()
     
     var body: some Scene {
         WindowGroup {
